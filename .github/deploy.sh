f2e594c822188b1b06f1d23631a12be31c7bf60c
dkhamsing <dkhamsing@cirruspath.com>
2016-05-13 11:33:17 -0700

    [ci] test

--- .github/deploy.sh
+++ .github/deploy.sh
@@ -5,14 +5,14 @@ set -e
 git config user.name "Travis CI"
 git config user.email "dkhamsing@users.noreply.github.com"
 
-git checkout master
+# git checkout master
 
 git add README.md
 git commit -m "[auto] [ci skip] Generate README"
 
-git add ARCHIVE.md
-git commit -m "[auto] [ci skip] Generate ARCHIVE"
+# git add ARCHIVE.md
+# git commit -m "[auto] [ci skip] Generate ARCHIVE"
 
-git status
+# git status
 
 git push --quiet "https://${GH_TOKEN}@github.com/dkhamsing/open-source-ios-apps" master:master > /dev/null 2>&1
