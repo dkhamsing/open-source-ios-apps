b38d273dea12b352fb83bc0118014c3e314787a2
Daniel Khamsing <dkhamsing8@gmail.com>
2017-08-11 15:33:31 -0700

    wip

--- osia.playground/Contents.swift
+++ osia.playground/Contents.swift
@@ -4,11 +4,9 @@ let fileURL = Bundle.main.url(forResource: "contents", withExtension: "json")
 let data = try Data(contentsOf: fileURL!)
 do {
     
-    let j: NSDictionary = try JSONSerialization.jsonObject(with: data as Data, options: JSONSerialization.ReadingOptions.mutableContainers) as! NSDictionary
-    
+    let j = try JSONSerialization.jsonObject(with: data as Data, options: JSONSerialization.ReadingOptions.mutableContainers) as! NSDictionary
     print (j.allKeys)
     
 } catch {
     
 }
-
