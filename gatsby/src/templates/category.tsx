e38fc9e8099b58ee047eae2a06bf5430901ea226
Callum Macdonald <git@callum-macdonald.com>
2020-09-08 16:57:44 +0200

    Render more category content.

--- gatsby/src/templates/category.tsx
+++ gatsby/src/templates/category.tsx
@@ -1,9 +1,29 @@
-import React from 'react'
-import SEO from '../components/seo'
-import Hero from '../components/hero'
+import {
+  Card,
+  CardContent,
+  createStyles,
+  Grid,
+  makeStyles,
+  Theme,
+  Typography,
+} from '@material-ui/core'
 import { graphql } from 'gatsby'
+import React from 'react'
+import Hero from '../components/hero'
+import SEO from '../components/seo'
+
+const useStyles = makeStyles((theme: Theme) =>
+  createStyles({
+    wrapper: {
+      paddingLeft: theme.spacing(2),
+      paddingRight: theme.spacing(2),
+    },
+  }),
+)
 
 const Category = props => {
+  const classes = useStyles()
+
   const category = props.data.appCategory
   const projectEdges = props.data.allAppProject.edges
   const projects = projectEdges.map(n => n.node)
@@ -13,13 +33,51 @@ const Category = props => {
       <SEO title="Home" />
       <Hero
         title={`Category: ${category.title}`}
-        description="A community curated set of open source iOS apps."
+        description={
+          category.description ||
+          'A community curated set of open source iOS apps.'
+        }
       ></Hero>
-      <ul>
-        {projects.map(p => {
-          return <li key={p.id}>title: {p.title}</li>
-        })}
-      </ul>
+      <div className={classes.wrapper}>
+        <Grid container spacing={2}>
+          {projects.map(project => {
+            return (
+              <Grid item xs={12} sm={6} md={4} key={project.id}>
+                <Card>
+                  <CardContent>
+                    <Typography>{project.title}</Typography>
+                    <Typography>Added: {project.date_added}</Typography>
+                    <Typography>Stars: {project.stars}</Typography>
+                    <Typography>{project.description}</Typography>
+                    <Typography>
+                      Source:{' '}
+                      <a href={project.source} target="_blank" rel="noreferrer">
+                        {project.source}
+                      </a>
+                    </Typography>
+                    <Typography>
+                      iTunes:{' '}
+                      <a href={project.itunes} target="_blank" rel="noreferrer">
+                        {project.itunes}
+                      </a>
+                    </Typography>
+                    <Typography>
+                      Homepage:{' '}
+                      <a
+                        href={project.homepage}
+                        target="_blank"
+                        rel="noreferrer"
+                      >
+                        {project.homepage}
+                      </a>
+                    </Typography>
+                  </CardContent>
+                </Card>
+              </Grid>
+            )
+          })}
+        </Grid>
+      </div>
     </>
   )
 }
