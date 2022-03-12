3597bc06ce85af57e94f956242cdd4c44c41c496
dkhamsing <dkhamsing@users.noreply.github.com>
2022-03-12 07:38:22 -0800

    Remove website (#1057)

--- gatsby/gatsby-ssr.js
+++
@@ -1,8 +0,0 @@
-/**
- * Implement Gatsby's SSR (Server Side Rendering) APIs in this file.
- *
- * See: https://www.gatsbyjs.org/docs/ssr-apis/
- */
-
-export { default as wrapRootElement } from './src/libs/wrapRootElement'
-export { default as wrapPageElement } from './src/libs/wrapPageElement'
